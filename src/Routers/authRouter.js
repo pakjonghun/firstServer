2eb61ab512754d5174c5b72bd046c3c9679862d5
coffeed-cat <gwcattower@gmail.com>
2021-07-28 09:35:54 +0900

    [ 여러가지 파일 ] 폴더 구조 변경 진행 중

--- src/Routers/authRouter.js
+++ src/Routers/authRouter.js
@@ -1,11 +1,31 @@
 import express from "express";
+
 import {
   changePassword,
   findPassword,
-} from "../Controllers/authControllers/password";
+} from "../Controllers/authControllers/passwordController";
+
+import {
+  oAuth,
+  oAuthJoin,
+} from "../Controllers/authControllers/oAuthController";
+
+import { join, login } from "../Controllers/authController/loginAndJoin";
 
 const authRouter = express.Router();
-authRouter.post("/password", findPassword);
+
+import joinValidator from "../middlewares/joinValidator";
+
+authRouter.post("/sociallogin", oAuth);
+
+authRouter.post("/socialjoin", oAuthJoin);
+
+authRouter.post("/join", joinValidator, join);
+
+authRouter.post("/login", login);
+
+authRouter.post("/findpassword", findPassword);
+
 authRouter.post("/changepassword", changePassword);
 
 export default authRouter;
--- src/app.js
+++ src/app.js
@@ -4,9 +4,7 @@ import "./api";
 import "./getDistance";
 
 import express from "express";
-import globalRouter from "./Routers/globalRouter";
 import detailRouter from "./Routers/detailRouter";
-import oAuthRouter from "./Routers/oAuthRouter";
 import authRouter from "./Routers/authRouter";
 
 const app = express();
@@ -14,11 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
-
-app.use("/", globalRouter);
 app.use("/detail", detailRouter);
-app.use("/oAuth", oAuthRouter);
 app.use("/auth", authRouter);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
